4f8c6137f120c56cedc82af6c565519a1bb60998
Dennis Wagelaar <dwagelaar@gmail.com>
2021-04-25 16:52:48 +0200

    Fix systemd service startup.

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ echo "Installing enforce-dpms"
 cp "$DIRNAME/enforce-dpms.sh" /usr/local/sbin/
 cp "$DIRNAME/enforce-dpms.service" /etc/systemd/user/
 systemctl daemon-reload
+echo "Please execute the following command:"
+echo "  systemctl --user daemon-reload"
---
+++ screen-log.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -ueo pipefail
+
+LOGFILE=${HOME}/screen-log-$(date +%Y%m%d-%H%M%S).csv
+DRM_DEVICE_PATH=/sys/class/graphics/fb0/device/drm/card0
+DRM_DEVICE_PORT=card0-HDMI-A-1
+echo "Writing to ${LOGFILE}" > /dev/stderr
+
+echo 'Timestamp;PowerSaveMode;card0-HDMI-A-1/dpms' | tee ${LOGFILE}
+
+while ( true ); do
+	echo -n "$(date '+%Y-%m-%d %H:%M:%S.%N');" | tee -a ${LOGFILE}
+	busctl --user get-property org.gnome.Mutter.DisplayConfig /org/gnome/Mutter/DisplayConfig org.gnome.Mutter.DisplayConfig PowerSaveMode | awk '{ORS=";"; print $0}' | tee -a ${LOGFILE}
+	cat ${DRM_DEVICE_PATH}/${DRM_DEVICE_PORT}/dpms | tee -a ${LOGFILE}
+	sleep 0.5
+done
