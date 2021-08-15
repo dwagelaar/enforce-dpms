14f5e11215ae7edbb709e775232f519cad832e09
Dennis Wagelaar <dwagelaar@gmail.com>
2021-08-15 17:13:41 +0200

    Log all ports

--- screen-log.sh
+++ screen-log.sh
@@ -2,15 +2,38 @@
 set -ueo pipefail
 
 LOGFILE=${HOME}/screen-log-$(date +%Y%m%d-%H%M%S).csv
-DRM_DEVICE_PATH=/sys/class/graphics/fb0/device/drm/card0
-DRM_DEVICE_PORT=card0-HDMI-A-1
+DRM_DEVICE_PATH=/sys/class/graphics/fb*/device/drm/card*
+ALL_DRM_PORTS=$(find ${DRM_DEVICE_PATH} | grep '/dpms$')
+
+function log_header()
+{
+	echo -n 'Timestamp;PowerSaveMode'
+	for DRM_PORT in ${ALL_DRM_PORTS}; do
+		echo -n "${DRM_PORT}" | sed -E 's/.*\/card[0-9]+\/(.*)\/dpms/;\1/g'
+	done
+	echo ""
+}
+
+function log_dpms_state()
+{
+	busctl --user get-property org.gnome.Mutter.DisplayConfig /org/gnome/Mutter/DisplayConfig org.gnome.Mutter.DisplayConfig PowerSaveMode | awk '{ORS=""; print $0}'
+}
+
+function log_drm_ports()
+{
+	for DRM_PORT in ${ALL_DRM_PORTS}; do
+		cat ${DRM_PORT} | awk '{ORS=""; print ";" $0}'
+	done
+}
+
 echo "Writing to ${LOGFILE}" > /dev/stderr
+exec &> >(tee -i ${LOGFILE})
 
-echo 'Timestamp;PowerSaveMode;card0-HDMI-A-1/dpms' | tee ${LOGFILE}
-
+log_header
 while ( true ); do
-	echo -n "$(date '+%Y-%m-%d %H:%M:%S.%N');" | tee -a ${LOGFILE}
-	busctl --user get-property org.gnome.Mutter.DisplayConfig /org/gnome/Mutter/DisplayConfig org.gnome.Mutter.DisplayConfig PowerSaveMode | awk '{ORS=";"; print $0}' | tee -a ${LOGFILE}
-	cat ${DRM_DEVICE_PATH}/${DRM_DEVICE_PORT}/dpms | tee -a ${LOGFILE}
+	echo -n "$(date '+%Y-%m-%d %H:%M:%S.%N');"
+	log_dpms_state
+	log_drm_ports
+	echo ""
 	sleep 0.5
 done
